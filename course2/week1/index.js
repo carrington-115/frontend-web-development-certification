9bd317712a337741e732f9b4eb0ab99bd3383b9b
Mark Carrington <frumarkcarrington@gmail.com>
2024-02-10 06:58:24 +0530

    used the switch case statement to check multiple conditions

--- course2/week1/index.js
+++ course2/week1/index.js
@@ -1,2 +1,13 @@
 console.log("hello world");
 // var name = 'mark'
+var newName;
+
+// using the swtich case and the if else conditional
+switch (newName) {
+  case "mark":
+    console.log("hello ", newName);
+    break;
+  default:
+    console.log("error occurred");
+    break;
+}
