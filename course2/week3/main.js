a2ee0735c13d128534200ddbd2555f475ec78365
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-04 07:00:21 +0530

    exploring functional programming in js

--- course2/week3/main.js
+++ course2/week3/main.js
@@ -1,1 +1,30 @@
 // this is week 3
+
+// 1. introduction to functional programming
+
+function currencyConverter(amount, rate) {
+  return amount * rate;
+}
+
+// const amountConverted = currencyConverter(100, 1.5);
+// console.log("Currency converted:", amountConverted);
+
+const currencyOperationsWithOOP = {
+  amount: 0,
+  converter: function (amount, rate) {
+    try {
+      return amount * rate;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  currency: "XAF",
+};
+
+const amount = 1500;
+const rate = 1.2;
+const newCurrency = "USD";
+
+const newConversion = currencyOperationsWithOOP.converter(amount, rate);
+currencyOperationsWithOOP.currency = newCurrency;
+console.log(newConversion, currencyOperationsWithOOP);
