5a1a3ea751c0ab4f786404470f86b4497f5e66eb
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-04 07:27:42 +0530

    learnt how to use the this keyword learnt scope in js functions

--- course2/week3/main.js
+++ course2/week3/main.js
@@ -11,9 +11,14 @@ function currencyConverter(amount, rate) {
 
 const currencyOperationsWithOOP = {
   amount: 0,
-  converter: function (amount, rate) {
+  converter: function (amount, rate, currency) {
     try {
-      return amount * rate;
+      if (typeof amount === "number" && typeof currency === "string") {
+        this.currency = currency;
+        return amount * rate;
+      } else {
+        throw new TypeError();
+      }
     } catch (error) {
       console.log(error);
     }
@@ -24,7 +29,12 @@ const currencyOperationsWithOOP = {
 const amount = 1500;
 const rate = 1.2;
 const newCurrency = "USD";
+console.log(typeof amount, typeof rate, typeof newCurrency);
 
-const newConversion = currencyOperationsWithOOP.converter(amount, rate);
-currencyOperationsWithOOP.currency = newCurrency;
-console.log(newConversion, currencyOperationsWithOOP);
+const newConversion = currencyOperationsWithOOP.converter(
+  amount,
+  rate,
+  newCurrency
+);
+console.log("Amount converted is:", newConversion);
+console.log(currencyOperationsWithOOP);
