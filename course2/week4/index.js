47a80e50c58eb5a20b66bb0341db2bb94cc99981
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-29 03:57:02 +0530

    install jest

---
+++ course2/week4/index.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+function add(num1, num2) {
+  //   if (typeof num1 !== number) {
+  //     Number(num1);
+  //   } else if (typeof num2 !== number) {
+  //     Number(num2);
+  //   }
+  return num1 + num2;
+}
+
+module.exports = add;
