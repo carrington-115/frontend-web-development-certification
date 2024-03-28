f7b8211fa312898b81cc9cb3f661146923f6eac0
Mark Carrington <frumarkcarrington@gmail.com>
2024-03-29 02:51:45 +0530

    learnt the basics of json with js code learnt how to convert from json to js and js to json

--- course2/week3/browser.js
+++ course2/week3/browser.js
@@ -1,5 +1,11 @@
+import data from "./data.json";
+
 console.log("hello world");
 
 export function printName(name) {
   console.log("My name is", name);
+}
+
+export function displayData() {
+  console.log(JSON.parse(data));
 }
